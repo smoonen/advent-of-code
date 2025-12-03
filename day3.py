f9e891c6db8f6cabd086da98cb07106d608b0bee
Scott Moonen <scott@moonen.us>
2025-12-03 06:20:51 -0500

    day 3 - final

---
+++ day3.py
@@ -0,0 +1,18 @@
+import functools, sys
+
+# Convert list of bank strings into list of list of numbers
+banks = list([bank.rstrip() for bank in sys.stdin.readlines()])
+
+#       v Sum the max value for each bank
+#              v For each bank, iterate through first char using range to generate index, and find the max combo
+#                    v find max possible jolt for each given index in this bank
+total = sum( [ max([ int(bank[x] + max(bank[x+1:])) for x in range(len(bank) - 1) ]) for bank in banks])
+
+print(f"Part 1: {total}")
+
+# For part 2 we have to go recursive. Memoize this to exchange memory for speed.
+jolt = functools.cache(lambda substring, depth : max([ substring[x] + ("" if depth == 1 else jolt(substring[x+1:], depth - 1)) for x in range(len(substring) + 1 - depth) ]))
+total = sum([int(jolt(bank, 12)) for bank in banks])
+
+print(f"Part 2: {total}")
+
